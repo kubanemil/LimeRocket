34843f014dd24c3b431bbbea4c0b904c36863e12
EmilKuban <emil3toktobekov@gmail.com>
2022-09-27 12:49:11 +0600

    started project in venv. Added requirements.

--- test2.py
+++ test2.py
@@ -1,14 +1,5 @@
-def At():
-    Pc = 2039000
-    y = 1.2
-    Tc = 2800
-    M = 0.02309
-    mdot = 1.754
-    R = 8.314/M
-    first = mdot / Pc
-    second = (R*Tc)/y
-    third = (2/(y+1))
-    fourth = (y+1)/(y-1)
-    return first * (second/(third**fourth))**0.5
+import matplotlib.pyplot as plt
+import numpy as np
+from engine import Engine
 
-print(At()*10000)
+eng = Engine()
